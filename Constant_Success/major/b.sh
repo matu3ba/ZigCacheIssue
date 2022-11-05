9fb61257b77f1fb1f160406f00d016632b37fd90
Jan Philipp Hafer <jan.hafer@rwth-aachen.de>
2022-11-06 00:20:47 +0100

    better scripts

--- Constant_Success/major/b.sh
+++ Constant_Success/major/b.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
-set -e
 
 CWD=$(pwd)
-trap "cd ${CWD}" EXIT HUP INT QUIT SIGSEGV TERM
+fail_cnt=0
+succ_cnt=0
+trap 'echo "succ_cnt: $succ_cnt, fail_cnt: $fail_cnt"' EXIT HUP QUIT SIGSEGV TERM
 
 i=1
-while [ ${i} -le 100 ]; do
+while [ ${i} -le 10 ]; do
   echo "STEP ${i}"
   ZIGCACHE=$(fd -uu zig-cache) && rm -fr "${ZIGCACHE}" && fd -uu zig-cache
   zig build test
+  ret_val=$?
+  test ${ret_val} -eq 0 && succ_cnt=$((succ_cnt+1))
+  test ${ret_val} -ne 0 && fail_cnt=$((fail_cnt+1))
 
   i=$(( i + 1 ))
 done
 
-echo "No failure after ${i} steps"
+echo "succ_cnt: $succ_cnt, fail_cnt: $fail_cnt"
 echo "cwd: ${CWD}"
+#TODO bell
